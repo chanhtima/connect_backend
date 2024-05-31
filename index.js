40b2b8d1246d393c1c6460c169b70679623c024a
akiko0311 <119967183+chanhtima@users.noreply.github.com>
2024-05-31 16:10:21 +0700

    add contact

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port =  3000;
 const connectDB = require('./config/db.js');
 const { readdirSync } = require('fs');
 const cors = require("cors");
